1695c6aafe113cb2cdde40be1cc675cde5a64536
ruchita352 <ruchiprajapati556@gmail.com>
2025-10-29 15:01:25 +0530

    javascript variiables

---
+++ Script.js
@@ -0,0 +1,55 @@
+//for printing
+console.log("heello wworld")
+
+//data types
+//1.number
+//2.string
+//3.booleans
+//4.undefined
+//5.null
+//6.object
+ 
+
+//too check data typee
+console.log(typeof("hello"))
+console.log(typeof(9))
+console.log(typeof(true))
+console.log(typeof(9))
+
+//variables
+//var
+//let
+//const-constaant its value can not bee changedd
+
+//ex....var
+//both possible reassignment and redeclaration
+var fname; //deeclarattion
+fname="mynaamme"//Assignment
+console.log(fname)
+fname=10
+console.log(fname)
+
+//let
+let gretting="say hi";
+console.log(gretting)
+let gretting1 = "say hello"
+console.log(gretting1)
+
+//const
+// var a="hii"
+// console.log(a)
+// var a=10
+// console.log(a)
+
+
+let count=10;
+count=20;
+
+
+let a=10;
+let b=20;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(10+"hello ")
